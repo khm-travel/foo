249729b49645739ce22a786fd0aa02bce08f4fc8
Julian Canepa <jjlcanepa@gmail.com>
2024-03-20 10:45:21 -0600

    Add page template example for iframes.

---
+++ wp-content/themes/foo/page-foo.php
@@ -0,0 +1,3 @@
+<?php /* Template Name: Iframe Example */ ?>
+<iframe src="http://unclaimed-commissions-app.test/" frameborder="0" width="100%" height="100%"></iframe>
+<style>body {margin: 0;}</style>
